e1d397613d0af8e0329272aa9112286fa2cec882
Allan Nava <allannava95@gmail.com>
2021-05-19 07:47:54 +0200

    update homefragment

--- app/src/main/java/com/example/androidviper/Home/HomeFragment.kt
+++ app/src/main/java/com/example/androidviper/Home/HomeFragment.kt
@@ -2,13 +2,13 @@ package com.example.androidviper.Home
 
 import android.content.Context
 import android.graphics.drawable.Drawable
+import com.example.androidviper.BaseNav.BaseNavFragment
 
 /**
  * Created by Allan Nava on 15/05/2021.
  * Updated by Allan Nava on 15/05/2021.
  */
-class HomeFragment : HomeContracts.View {
-
+class HomeFragment : BaseNavFragment(), HomeContracts.View {
 
 
     override fun showLoadingData(isLoading: Boolean) {
